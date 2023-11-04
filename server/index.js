93099b93d13e369698e322814fbabe1c6a4c1128
Orson <55061548+0carb0n@users.noreply.github.com>
2023-11-03 23:21:46 -0400

    Updated server to support https.

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
+const http = require('node:http');
+const https = require('node:https');
+const fs = require('node:fs');
+
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -29,5 +33,17 @@ app.use('/api', apiRouter)
 const db = require('./db')
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-// PUT THE SERVER IN LISTENING MODE
-app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`))
+if (process.env.ENVIRONMENT === "PRODUCTION") {
+    const options = {
+        key: fs.readFileSync('../ssl/privkey.pem'),
+        cert: fs.readFileSync('../ssl/fullchain.pem'),
+    };
+
+    const httpApp = http.createServer(app);
+    const httpsApp = https.createServer(options, app).listen(PORT, () => {
+        console.log("Express server listening on port " + PORT);
+    });
+} else if (process.env.ENVIRONMENT === "DEVELOPMENT") {
+    // PUT THE SERVER IN LISTENING MODE
+    app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`))
+}
