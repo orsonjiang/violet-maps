9f11a2d08ca6b5c303e4839177efc82fd79ceb07
Orson <55061548+0carb0n@users.noreply.github.com>
2023-11-03 22:36:10 -0400

    Testing CI/CD for server.

--- server/routes/api-router.js
+++ server/routes/api-router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const ApiController = require("../controllers/api-controller");
 
-router.get("/", () => {
+router.get("/", (req, res) => {
 	res.status(200).json({
 		message: "test 2 success!"
 	})
