c3e5789bbf9f261f89629d27a48e3430ebe94836
Kayla Fang <kayfang182@gmail.com>
2023-11-06 05:50:19 -0500

    added fanny's add comment jest test

--- tests/backend/comment.test.js
+++ tests/backend/comment.test.js
@@ -18,6 +18,35 @@ describe("get comments", () => {
     });
 });
 
+describe('add comment', () => {
+    it('should add a comment in db', async() => {
+        const getComment = await request(app).get('/api/comments')
+
+        expect(getComment.statusCode).toBe(200);
+
+        const numComments = getComment.body.comments.length;
+
+        const addRes = await request(app).post('/api/comments/add')
+            .send({content: "Jest test to add comments"})
+
+        expect(addRes.statusCode).toBe(200);
+
+        const getNewComment = await request(app).get('/api/comments')
+
+        expect(getNewComment.statusCode).toBe(200);
+        expect(getNewComment.body.comments.length).toEqual(numComments + 1);
+        expect(getNewComment.body.comments[numComments].content).toEqual("Jest test to add comments");
+
+        const commentID = getNewComment.body.comments[numComments]._id;
+
+        return request(app)
+            .delete('/api/comments/delete/' + commentID)
+            .expect(200)
+
+
+    })
+})
+
 describe("edit comment", () => {
     test("PUT /comments/edit/:_id", async () => {
         // add comment
