95153bab6c58fa58d8ae3189219225462fd2fda0
Fanny Li <fannyli704@gmail.com>
2023-11-10 16:11:12 -0500

    working on selected map screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import SiteWrapper from './pages/components/SiteWrapper';
 import Home from './pages/Home/Home';
+import SelectedScreen from './pages/Selected/SelectedScreen';
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <SiteWrapper>
                 <Routes>
                     <Route path="/*" element={<Home/>} />
+                    <Route path="/selected" element={<SelectedScreen/>} />
                 </Routes>
             </SiteWrapper>
         </BrowserRouter>
