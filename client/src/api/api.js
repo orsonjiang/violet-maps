a5f133310df60c0cf79e25e04191ccfeed8b1709
Orson <55061548+0carb0n@users.noreply.github.com>
2023-11-10 14:37:06 -0500

    Removed code for Build 1 testing.

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -7,20 +7,4 @@ const api = axios.create({
 	baseURL: `${import.meta.env.VITE_SERVER_URL}/api`
 })
 
-const getComments = () => api.get(`/comments`);
-const addComment = (content) => api.post("/comments/add", {
-	content: content
-});
-const editComment = (_id, content) => api.put("/comments/edit/" + _id, {
-	content: content
-});
-const deleteComment = (_id) => api.delete("/comments/delete/" + _id);
-
-const apis = {
-	getComments,
-	addComment,
-	editComment,
-	deleteComment,
-};
-
 export default apis;
--- client/src/helpers/fetch.js
+++ client/src/helpers/fetch.js
@@ -1,8 +1,2 @@
 import api from "../api/api";
 import store from "../store";
-import { setComments } from "../actions";
-
-export const fetchComments = async () => {
-	let res = await api.getComments();
-	store.dispatch(setComments(res.data.comments))
-};
