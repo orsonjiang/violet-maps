0dddb65bee1574a48f81405844bae8ca7d2179a8
Fanny Li <fannyli704@gmail.com>
2023-11-10 16:44:33 -0500

    styled splash screen

--- client/src/pages/Splash/components/TitleButton.jsx
+++ client/src/pages/Splash/components/TitleButton.jsx
@@ -1,7 +1,7 @@
 const TitleButton = ({title, buttonText}) => {
     return (<div className="flex flex-col my-4">
-        <div className="text-2xl my-2">{title}</div>
-        <button className="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-xl px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 self-center">
+        <div className="text-xl my-2 font-semibold text-center">{title}</div>
+        <button className="text-white bg-[#B998EE] hover:bg-[#9479BE] focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-md  py-2 text-center mb-2 dark:bg-[#B998EE]dark:hover:bg-purple-700 dark:focus:ring-purple-900 self-center w-60">
 			{buttonText}
         </button>
     </div>);
--- client/src/pages/Splash/index.jsx
+++ client/src/pages/Splash/index.jsx
@@ -3,27 +3,27 @@ import TitleButton from "./components/TitleButton";
 const Splash = () => {
     return (
         <div className="flex min-h-screen">
-            <div className="flex flex-col p-32 justify-center">
-                <div className="text-6xl my-8">Violet Maps</div>
-                <div className="text-2xl min-w-xl">
+            <div className="flex flex-col p-32 justify-center bg-gradient-to-b from-[#B998EE] to-[#F3E7FF]">
+                <div className="font-bold text-6xl my-8 text-white">Violet Maps</div>
+                <div className="text-2xl min-w-xl text-[#F3E7FF]">
                     Connect with other map enthusiasts around you on Violet
                     Maps.
                 </div>
-                <div className="text-xl my-12 bg-white rounded-lg p-4">
+                <div className="text-lg my-12 bg-white rounded-lg px-8 py-10">
                     <ul>
-                        <li>Customize your very own map</li>
-                        <li>Add custom labels</li>
-                        <li>Color code countries or states</li>
-                        <li>
+                        <li className="text-[#9479BE] font-medium">Customize your very own map</li>
+                        <li className="text-[#9479BE] font-medium">Add custom labels</li>
+                        <li className="text-[#9479BE] font-medium">Color code countries or states</li>
+                        <li className="text-[#9479BE] font-medium">
                             Publish your maps to others or keep them private for
                             personal viewing
                         </li>
-                        <li>View, like and comment on other maps</li>
-                        <li>Free and easy to use </li>
+                        <li className="text-[#9479BE] font-medium">View, like and comment on other maps</li>
+                        <li className="text-[#9479BE] font-medium"> Free and easy to use </li>
                     </ul>
                 </div>
             </div>
-            <div className="flex flex-col p-8 grow justify-center items-center">
+            <div className="flex flex-col p-8 grow justify-center items-center bg-white">
                 <TitleButton title={"Don't have an account?"} buttonText={"Sign Up"} />
                 <TitleButton title={"Already have an account?"} buttonText={"Log In"} />
                 <TitleButton title={"Want to anonymously browse?"} buttonText={"Continue as Guest"} />
