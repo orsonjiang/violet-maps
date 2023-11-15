739bcb1b978d0dfbfac50a7c9c312c4672f99955
Orson <55061548+0carb0n@users.noreply.github.com>
2023-11-15 09:46:23 -0500

    Removed old API calls.

--- server/routes/api-router.js
+++ server/routes/api-router.js
@@ -5,9 +5,5 @@ const ApiController = require("../controllers/api-controller");
 router.get("/", (req, res) => {
 	res.status(200).json({message:"test 2 success!"})
 });
-router.get("/comments", ApiController.getComment);
-router.post("/comments/add", ApiController.addComment);
-router.put("/comments/edit/:_id", ApiController.editComment);
-router.delete("/comments/delete/:_id", ApiController.deleteComment);
 
 module.exports = router;
