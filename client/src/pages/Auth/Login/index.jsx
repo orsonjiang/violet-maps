e1c115ce4cf6aceda1b4f268ddc98700c480340d
Orson <55061548+0carb0n@users.noreply.github.com>
2023-11-16 22:15:37 -0500

    Created Login and Register.

--- client/src/pages/Auth/Login/index.jsx
+++ client/src/pages/Auth/Login/index.jsx
@@ -1,74 +1,63 @@
 import { Link } from 'react-router-dom';
 
+import auth from '../../../api/auth';
+import store from '../../../store';
+import { setUser } from '../../../actions/user';
+import Form from '../components/Form';
+import Field from '../components/Field';
+
 const Login = () => {
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+
+        const data = {
+            email: formData.get('email'),
+            password: formData.get('password'),
+        };
+
+        const req = await auth.postLogin(data);
+
+        if (req.status === 200) {
+            store.dispatch(setUser(req.data));
+        } else {
+            console.log(req.error);
+        }
+    };
+
     return (
-        <div className="flex flex-col min-h-screen bg-violet-200 justify-center">
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <div className="w-[60rem] h-[34rem] bg-white rounded-lg shadow-xl mt-0 max-w-md xl:p-0 ">
-                    <div className="p-6 space-y-4 m-8 ">
-                        <h1
-                            href="#"
-                            className="text-2xl text-center text-indigo-500 font-bold font-italic leading-tight tracking-none "
-                        >
-                            Violet Maps
-                        </h1> 
-                        <h2 className="text-center pb-5 font-semibold leading-none tracking-none text-gray-900 text-md ">
-                            Sign in to your account
-                        </h2>
-                        <form className="space-y-4 md:space-y-4" action="#">
-                            <div>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    id="email"
-                                    className="bg-gray-50  text-gray-900 shadow-md sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                                    placeholder="Email or username"
-                                    required=""
-                                />
-                            </div>
-                            <div>
-                                <input
-                                    type="password"
-                                    name="password"
-                                    id="password"
-                                    placeholder="Password"
-                                    className="bg-gray-50 text-gray-900 shadow-md sm:text-sm rounded-lg focus:border-violet-600 block w-full p-2.5 "
-                                    required=""
-                                />
-                            </div>
-                            <div className="pt-5 text-sm text-black font-semibold ">
-                                Forgot your{' '}
-                                <Link to={"/requestReset"}
-                                    className="font-semibold text-violet-400 hover:underline "
-                                >
-                                    password?
-                                </Link>
-                            </div>
-                            <div className="text-sm font-semibold text-black ">
-                                New to Violet Maps?{' '}
-                                <Link to={"/register"}
-                                    className="font-semibold text-violet-400 hover:underline "
-                                >
-                                    Sign up
-                                </Link>
-                            </div>
-                            <div className='grid grid-cols-2 grid-row-1 my-4'>
-                                <div className='col-span-2 flex space-x-2 justify-end text-sm'>
-                                    <Link to={"/app/home"}>
-                                        <button
-                                            type="submit"
-                                            className="rounded-full text-white bg-[#8187DC] py-1.5 px-6 shadow-lg focus:outline-none focus:ring-2 focus:ring-purple-300 font-medium text-md text-center"
-                                        >
-                                            Log in
-                                        </button>
-                                    </Link>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+        <Form title="Sign in to your account" onSubmit={handleLogin}>
+            <Field id="email" placeholder="Email" />
+            <Field id="password" placeholder="Password" type="password" />
+            <div className="flex justify-start pt-2 pb-4">
+                <Link
+                    to={'/requestReset'}
+                    className="text-sm text-gray-500 font-medium hover:underline"
+                >
+                    Forgot password?
+                </Link>
             </div>
-        </div>
+            <div className="flex flex-col w-full gap-6">
+                <button
+                    type="submit"
+                    className="rounded-full text-white bg-[#8187DC] py-2 shadow-md focus:outline-none focus:ring-2 focus:ring-purple-300 font-medium text-center"
+                >
+                    Log in
+                </button>
+                <div className="flex items-center">
+                    <hr className="flex-grow border-t border-gray-300" />
+                    <div className="px-4">OR</div>
+                    <hr className="flex-grow border-t border-gray-300" />
+                </div>
+                <Link
+                    to={'/register'}
+                    className="rounded-full text-white bg-[#8187DC] py-2 shadow-md hover:outline-none hover:ring-2 hover:ring-purple-300 font-medium text-center"
+                >
+                    <button>Sign Up</button>
+                </Link>
+            </div>
+        </Form>
     );
 };
 
--- client/src/pages/Auth/Register/index.jsx
+++ client/src/pages/Auth/Register/index.jsx
@@ -1,73 +1,65 @@
 import { Link } from 'react-router-dom';
 
-import Field from './components/Field';
+import auth from '../../../api/auth';
+import store from '../../../store';
+import { setUser } from '../../../actions/user';
+import Form from '../components/Form';
+import Field from '../components/Field';
 
 const Register = () => {
-    const handleRegister = (e) => {
+    const handleRegister = async (e) => {
         e.preventDefault();
 
         const formData = new FormData(e.currentTarget);
 
         const data = {
-            firstName: formData.get("firstName"),
-            lastName: formData.get("lastName"),
-            email: formData.get("email"),
-            username: formData.get("username"),
-            password: formData.get("password"),
+            firstName: formData.get('firstName'),
+            lastName: formData.get('lastName'),
+            email: formData.get('email'),
+            username: formData.get('username'),
+            password: formData.get('password'),
+        };
+
+        if (data.password !== formData.get('confirmPassword')) {
+            console.log('no duplicate password');
+            return;
         }
 
-        if (data !== formData.get("confirmPassword")) {
-            // Error modal.
+        const req = await auth.postRegister(data);
+        if (req.status === 200) {
+            store.dispatch(setUser(req.data));
+        } else {
+            console.log(req.error);
         }
-
-        console.log(data)
     };
 
     return (
-        <div className="flex flex-col bg-violet-200 min-h-screen justify-center">
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <div className="flex items-center justify-center w-[60rem] h-[34rem] bg-white rounded-lg shadow-xl mt-0 max-w-md xl:p-0 ">
-                    <div className="p-6 space-y-4 m-8">
-                        <div
-                            className="text-2xl text-center text-indigo-500 font-bold font-italic leading-tight tracking-none "
-                        >
-                            Violet Maps
-                        </div>
-                        <h2 className="text-center pb-5 font-semibold leading-none tracking-none text-gray-900 text-md ">
-                            Create your new account
-                        </h2>
-                        <form className="space-y-4" onSubmit={handleRegister}>
-                            <div className="flex gap-4">
-                                <Field id="firstName" placeholder="First name"/>
-                                <Field id="lastName" placeholder="Last name"/>
-                            </div>
-                            <Field id="email" placeholder="Email"/>
-                            <Field id="username" placeholder="Username"/>
-                            <Field id="password" placeholder="Password"/>
-                            <Field id="confirmPassword" placeholder="Confirm password"/>
-                            <p className="text-sm font-semibold text-black pt-5">
-                                Already have an account?{' '}
-                                <Link to={"/login"}
-                                    className="font-semibold text-violet-400 hover:underline "
-                                >
-                                    Sign in
-                                </Link>
-                            </p>
-                            <div className='grid grid-cols-2 grid-row-1 my-4'>
-                                <div className='col-span-2 flex space-x-2 justify-end text-sm'>
-                                    <button
-                                        type="submit"
-                                        className="text-white bg-[#8187DC] rounded-full py-1.5 px-6 shadow-lg text-center focus:outline-none focus:ring-2 focus:ring-purple-300 font-medium"
-                                    >
-                                        Register
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+        <Form title="Create your new account" onSubmit={handleRegister}>
+            <div className="flex gap-4">
+                <Field id="firstName" placeholder="First name" />
+                <Field id="lastName" placeholder="Last name" />
             </div>
-        </div>
+            <Field id="email" placeholder="Email" />
+            <Field id="username" placeholder="Username" />
+            <Field id="password" placeholder="Password" type="password" />
+            <Field
+                id="confirmPassword"
+                placeholder="Confirm password"
+                type="password"
+            />
+            <p className="text-sm font-semibold text-black py-2">
+                Already have an account?{' '}
+                <Link
+                    to={'/login'}
+                    className="font-semibold text-violet-400 hover:underline "
+                >
+                    Sign in
+                </Link>
+            </p>
+            <button className="rounded-full text-white bg-[#8187DC] py-2 shadow-md hover:outline-none hover:ring-2 hover:ring-purple-300 font-medium text-center">
+                Sign Up
+            </button>
+        </Form>
     );
 };
 
