21833cbd80aabccd552926095c88c4c13d7a4394
Kayla Fang <kayfang182@gmail.com>
2023-11-18 14:42:09 -0500

    adjusted spacing in profile bubble

--- client/src/pages/App/Edit/index.jsx
+++ client/src/pages/App/Edit/index.jsx
@@ -47,10 +47,6 @@ const EditMap = () => {
 
     return (
         <div className="m-4 text-[13px]">
-            {/* {modal == "text" ? 
-                <Modal title={"Add/Edit Label for Region"} description={"Enter in value for the label"} containsInput={true} /> 
-                ? (modal == "dataProp" ? <Modal title={"Add New Data Property"} description={"Enter a name for your property"} containsInput={true}/> :
-             : ""} */}
             {modal ? openModal() : ""}
             <div className="flex gap-4 my-5 text-2xl font-bold justify-center items-center">
                 Map of Europe
@@ -61,11 +57,6 @@ const EditMap = () => {
             <Toolbar />
             <div className="w-full p-4 rounded">
                 <div id="map" className="h-[60vh]"></div>
-                {/* <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/A_large_blank_world_map_with_oceans_marked_in_blue.PNG/2560px-A_large_blank_world_map_with_oceans_marked_in_blue.PNG"
-                    alt="map-image"
-                    className="rounded-lg"
-                /> */}
             </div>
             <div className="flex gap-3 items-center mx-3 my-3">
                 <div className="text-white bg-violet-400 hover:bg-violet-500 focus:outline-none rounded-full px-4 py-1.5 text-center mb-2 ">
