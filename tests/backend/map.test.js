9f9a232197afff1b6771e7216e9f27a173b1d6cc
Kayla Fang <kayfang182@gmail.com>
2023-11-20 08:08:57 -0500

    fix package.json

--- tests/backend/map.test.js
+++ tests/backend/map.test.js
@@ -1,28 +1,25 @@
-// const request = require('supertest');
+const request = require('supertest');
 
-// const { app, db } = require('../../server');
+const { app, db } = require('../../server');
 
-// const testData = require('./test-files/custom.geo.json');
+const testData = require('./test-files/custom.geo.json');
 
-// afterAll(() => {
-//     db.close();
-// })
+afterAll(() => {
+    db.close();
+})
 
 
-// describe("create map", () => {
-//     test("POST /api/map", async () => {
-//         console.log(testData);
-//         return request(app).post("/api/map").send({
-//             newMap: {
-//                 name: "Great Map",
-//                 data: {...testData},
-//                 template: "numerical",
-//                 dataProperty: "admin_0",
-//                 color: "purple"
-//             },
-//         }).expect(201).then((res) => {
-//             console.log(res.body)
-//         })
-//     })
+describe("create map", () => {
+    test("POST /api/map", async () => {
+        return request(app).post("/api/map").send({
+            name: "Great Map",
+            data: {...testData},
+            template: "numerical",
+            dataProperty: "admin_0",
+            color: "purple"
+        }).expect(201).then((res) => {
+            console.log(res.body)
+        })
+    })
 
-// })
+})
