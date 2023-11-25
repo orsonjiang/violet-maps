4d9fdf507d3f2cd4b7e8d02f5c460b71d1da3801
Kayla Fang <kayfang182@gmail.com>
2023-11-25 00:41:47 -0500

    route to home if current map is null - selected map screen

--- client/src/pages/App/Map/index.jsx
+++ client/src/pages/App/Map/index.jsx
@@ -4,13 +4,15 @@ import { useState, useRef, useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { openModal } from "../../../actions/modal";
 import * as L from 'leaflet';
-import "../../../dist/leaflet.browser.print.min.js"
+// import "../../../dist/leaflet.browser.print.min.js"
 import { setView } from "../../../actions/home";
 import { addComment } from "../../../actions/map.js";
 import apis from "../../../api/api.js";
+import { useNavigate } from "react-router-dom";
 
 const Map = () => {
     const map = useRef(null);
+    const navigate = useNavigate();
     const [menu, setMenu] = useState("none");
     const [text, setText] = useState("");
 
@@ -60,6 +62,9 @@ const Map = () => {
 
     useEffect(() => {
         dispatch(setView("NONE"));
+        if (currentMap == null) {
+            navigate("/app/home"); // for now
+        }
         if (!map.current) {
             map.current = L.map('map').setView([39.74739, -105], 2);
 
@@ -139,66 +144,69 @@ const Map = () => {
     )
 
     return (
-        <div className="md:grid grid-cols-3 gap-5 m-10 pb-10 max-md:block">
-            {currentModal == "FORK_MODAL" ? <Modal title={"Fork Map?"} description={"Confirm by typing a name for the Map of Europe"} inputText={"Enter Map Name"} containsInput={true} /> : ""}
-            <div className='col-span-2'>
-                <div id="map" className="h-[60vh] rounded-lg shadow-md"></div>
-                <div className='grid grid-cols-3 grid-row-2 my-4'>
-                    <div className='col-span-1 row-span-2'>
-                        <h3 className='font-semibold text-lg'>{currentMap.name}</h3>
-                        <h4 className="">{currentMap.username}</h4>
-                        <div className="flex gap-3 items-center mt-3 text-xs whitespace-nowrap">
-                    
-                            {currentMap.tags.length != 0 ? currentMap.tags.map((name) => {
-                                return ( <div className=" bg-violet-200 focus:outline-none rounded-full px-4 py-1.5 text-center mb-2 ">
-                                    {name}
-                                </div>)
-                            }) : (<div className="text-xs text-gray-300">No tags</div>)}
+        <div>
+            {currentMap ? 
+            <div className="md:grid grid-cols-3 gap-5 m-10 pb-10 max-md:block">
+                {currentModal == "FORK_MODAL" ? <Modal title={"Fork Map?"} description={"Confirm by typing a name for the Map of Europe"} inputText={"Enter Map Name"} containsInput={true} /> : ""}
+                <div className='col-span-2'>
+                    <div id="map" className="h-[60vh] rounded-lg shadow-md"></div>
+                    <div className='grid grid-cols-3 grid-row-2 my-4'>
+                        <div className='col-span-1 row-span-2'>
+                            <h3 className='font-semibold text-lg'>{currentMap.name}</h3>
+                            <h4 className="">{currentMap.username}</h4>
+                            <div className="flex gap-3 items-center mt-3 text-xs whitespace-nowrap">
+                        
+                                {currentMap.tags.length != 0 ? currentMap.tags.map((name) => {
+                                    return ( <div className=" bg-violet-200 focus:outline-none rounded-full px-4 py-1.5 text-center mb-2 ">
+                                        {name}
+                                    </div>)
+                                }) : (<div className="text-xs text-gray-300">No tags</div>)}
+                            </div>
+                        </div>
+                        <div className='col-span-2 flex space-x-2 justify-end text-xs font-medium flex-wrap'>
+                            <button className='rounded-full bg-accent py-1.5 px-4 shadow-lg text-white'><i className="fa-solid fa-thumbs-up pr-2"></i>{currentMap.social.likes}</button>
+                            <button className='rounded-full bg-accent py-1.5 px-4 shadow-lg text-white'><i className="fa-solid fa-thumbs-down pr-2"></i>{currentMap.social.dislikes}</button>
+                            <div className="flex relative">
+                                <button onClick={() => { setMenu("export") }} className='rounded-full bg-accent py-1.5 px-4 shadow-lg text-white'>
+                                    <i class="fa-solid fa-file-export pr-2"></i>
+                                    Export
+                                </button>
+                                {menu == "export" ? exportMenu : null}
+                            </div>
+                            <button className='rounded-full bg-accent py-1.5 px-4 shadow-lg text-white' onClick={() => { openCurrentModal("FORK_MODAL") }}><i class="fa-solid fa-copy pr-2" ></i>Fork</button>
                         </div>
                     </div>
-                    <div className='col-span-2 flex space-x-2 justify-end text-xs font-medium flex-wrap'>
-                        <button className='rounded-full bg-accent py-1.5 px-4 shadow-lg text-white'><i className="fa-solid fa-thumbs-up pr-2"></i>{currentMap.social.likes}</button>
-                        <button className='rounded-full bg-accent py-1.5 px-4 shadow-lg text-white'><i className="fa-solid fa-thumbs-down pr-2"></i>{currentMap.social.dislikes}</button>
-                        <div className="flex relative">
-                            <button onClick={() => { setMenu("export") }} className='rounded-full bg-accent py-1.5 px-4 shadow-lg text-white'>
-                                <i class="fa-solid fa-file-export pr-2"></i>
-                                Export
+
+                </div>
+                <div className='col-span-1 bg-violet-100 rounded-lg self-start'>
+                    <div className="m-5 mb-1">
+                        <h3 className="font-medium pt-5 md:pt-0">{`${currentMap.social.comments.length} Comments`}</h3>
+                        <div className="mt-3 flex space-x-4">
+                            <button className="flex gap-[1px] items-center justify-center h-10 w-10 shadow-none hover:shadow-none font-semibold bg-indigo-200 text-xs p-2 rounded-full shrink-0">
+                                <p>{user.firstName.charAt(0)}</p>
+                                <p>{user.lastName.charAt(0)}</p>
                             </button>
-                            {menu == "export" ? exportMenu : null}
+                            <input
+                                type="search"
+                                id="search-dropdown"
+                                className="block px-3 w-full text-sm rounded-lg drop-shadow-sm focus:outline-none focus:ring-2"
+                                placeholder="Add a comment..."
+                                required=""
+                                ref={ref}
+                                onChange={handleUpdateText}
+                                onKeyDown={handleAddComment}
+                            />
                         </div>
-                        <button className='rounded-full bg-accent py-1.5 px-4 shadow-lg text-white' onClick={() => { openCurrentModal("FORK_MODAL") }}><i class="fa-solid fa-copy pr-2" ></i>Fork</button>
+                        <div className="overflow-hidden hover:overflow-y-scroll max-h-[30rem] mt-3 space-y-2">
+                            {currentMap.social.comments.map((c) => {
+                                return <CommentCard initials={c.userInitial} name={c.username} comment={c.comment} />
+                            })}
+
+                        </div>
+
                     </div>
                 </div>
-
-            </div>
-            <div className='col-span-1 bg-violet-100 rounded-lg self-start'>
-                <div className="m-5 mb-1">
-                    <h3 className="font-medium pt-5 md:pt-0">{`${currentMap.social.comments.length} Comments`}</h3>
-                    <div className="mt-3 flex space-x-4">
-                        <button className="flex gap-[1px] items-center justify-center h-10 w-10 shadow-none hover:shadow-none font-semibold bg-indigo-200 text-xs p-2 rounded-full shrink-0">
-                            <p>{user.firstName.charAt(0)}</p>
-                            <p>{user.lastName.charAt(0)}</p>
-                        </button>
-                        <input
-                            type="search"
-                            id="search-dropdown"
-                            className="block px-3 w-full text-sm rounded-lg drop-shadow-sm focus:outline-none focus:ring-2"
-                            placeholder="Add a comment..."
-                            required=""
-                            ref={ref}
-                            onChange={handleUpdateText}
-                            onKeyDown={handleAddComment}
-                        />
-                    </div>
-                    <div className="overflow-hidden hover:overflow-y-scroll max-h-[30rem] mt-3 space-y-2">
-                        {currentMap.social.comments.map((c) => {
-                            return <CommentCard initials={c.userInitial} name={c.username} comment={c.comment} />
-                        })}
-
-                    </div>
-
-                </div>
-            </div>
+            </div> : null}
         </div>
     );
 };
