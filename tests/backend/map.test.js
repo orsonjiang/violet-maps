84fc2b26f10bae980dbf9ac7d8c32775936cc35d
Fanny Li <fannyli704@gmail.com>
2023-11-27 11:13:24 -0500

    add comment and delete map jest test

--- tests/backend/map.test.js
+++ tests/backend/map.test.js
@@ -9,7 +9,9 @@ afterAll(() => {
 })
 
 
-describe("create map", () => {
+describe("create and delete map", () => {
+    var mapId;
+
     test("POST /api/map", async () => {
         return request(app).post("/api/map").send({
             name: "Great Map",
@@ -21,8 +23,24 @@ describe("create map", () => {
             features: []
         }).expect(201).then((res) => {
             console.log(res.body)
+            mapId = res.body.id;
         })
     })
+
+    test("DELETE /api/map/:id", async() => {
+        return request(app).delete(`/api/map/${mapId}`).send({
+            name: "Great Map",
+            data: { ...testData },
+            username: "kfang00",
+            template: "string",
+            dataProperty: "admin",
+            color: "purple",
+            features: []
+        }).expect(200).then((res) => {
+            console.log(res.body);
+        })
+    })
+
 })
 
 describe("get maps", () => {
@@ -40,14 +58,10 @@ describe("get maps", () => {
     })
 })
 
-describe("get map by id", () => {
-    test("GET /api/map/:id", async () => {
-        return request(app).get(`/api/map/6564534417cd89c108b05e70`)
-        .expect(200).then((res) => {
-            console.log(res.body)
-            expect(res.body).toHaveProperty('success');
-            expect(res.body).toHaveProperty('map');
-        })
+
+describe("delete map by id", () => {
+    test("DELETE /api/map/:id", async() => {
+        return request(app).delete("")
     })
 })
 
@@ -74,25 +88,32 @@ describe("update map", () => {
         })
     })
 
-    // test("add comment", async() => {
-    //     return request(app).put("/api/map/6564534417cd89c108b05e70").send({
-    //         map: {
-    //             social: {
-    //                 comments: {
-    //                     comment: "Testing add comment.",
-    //                     userReference: "someUserId",
-    //                     username: "TestUser",
-    //                     userInitial: "TU",
-    //                     datePublished: new Date()
-    //                 }
-    //             }
-    //         }
-    //     }).expect(200).then((res) => {
-    //         console.log(res.body);
-    //         expect(res.body).toHaveProperty('id');
-    //         expect(res.body).toHaveProperty('successMessage');
-    //     })
-    // })
+    test("add comment", async() => {
+        return request(app).put("/api/map/6564534417cd89c108b05e70").send({
+            map: {
+                social: {
+                    comments: [{
+                        comment: "Testing add comment.",
+                        userReference: "someUserId",
+                        username: "TestUser",
+                        userInitial: "TU",
+                        datePublished: new Date()
+                    }],
+                    likes: 0,
+                    views: 0,
+                    dislikes: 0,
+                },
+                graphics: {
+                    showLabels: false,
+                    dataProperty: "admin"
+                }
+            }
+        }).expect(200).then((res) => {
+            console.log(res.body);
+            expect(res.body).toHaveProperty('id');
+            expect(res.body).toHaveProperty('successMessage');
+        })
+    })
 
     test("unpublish map", async () => {
         return request(app).put("/api/map/6564534417cd89c108b05e70").send({
@@ -117,6 +138,8 @@ describe("update map", () => {
     })
 
 })
+
+
 
 
 
