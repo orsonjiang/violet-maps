1597cb4b4b21334be93b86155f8499f0f69141d4
Orson <55061548+0carb0n@users.noreply.github.com>
2023-11-30 12:43:07 -0500

    Styling changes.

--- client/src/pages/components/SiteWrapper.jsx
+++ client/src/pages/components/SiteWrapper.jsx
@@ -1,7 +1,6 @@
 const SiteWrapper = ({children}) => {
     return (
         <div className='flex-col bg-gray-50 min-h-screen w-auto'>
-            {/* <div className='bg-gradient-to-r from-gray-50 to-gray-50 min-h-screen'></div> */}
             {children}
         </div>
     )
