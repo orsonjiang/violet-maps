f55a2428f966ed7db046b0b81bc7105f6b836707
Orson <55061548+0carb0n@users.noreply.github.com>
2023-11-30 16:15:33 -0500

    Updated MapSchema.

--- server/models/MapSchema.js
+++ server/models/MapSchema.js
@@ -1,62 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-// const GeoJSON = require('mongoose-geojson-schema');
-
-// const MapSchema = new Schema(
-//     {
-//         name: { type: String, required: true },
-//         ownerId: { type: String, required: true },
-//         tags: [String],
-//         publishedDate: { type: Date, required: true },
-//         data: {},
-//         graphics: {
-//             fontStyle: { type: String, required: true },
-//             fontSize: { type: Number, required: true },
-//             labelPosition: { type: String, required: true },
-//             dataProperty: { type: String, required: true },
-//             heatMap: {
-//                 dataProperty: { type: String },
-//             },
-//             showLabels: { type: Boolean, required: true },
-//             bubbles: {
-//                 dataProperty: {type: String},
-//             },
-//             legend: {
-//                 name: { type: String },
-//                 position: { type: String },
-//                 value: [
-//                     {
-//                         color: String,
-//                         description: String
-//                     }
-//                 ],
-//                 visible: { type: Boolean, required: true }
-//             }
-//         },
-//         social: {
-//             views: { type: Number, required: true },
-//             likes: { type: Number, required: true },
-//             dislikes: { type: Number, required: true },
-//             comments: [
-//                 {
-//                     comment: String,
-//                     userReference: String,
-//                     datePublished: Date
-//                 }
-//             ]
-//         }
-//     },
-//     { timestamps: true }
-// );
 
 const MapSchema = new Schema(
     {
         name: { type: String, required: true },
-        username: { type: String, required: true },
+        ownerId: { type: String, required: true },
         tags: [String],
-        publishedDate: { type: Date },
-        creationDate: { type: Date, required: true },
-        data: {type: Object, required: true },
+        publishedDate: { type: Date, required: true },
+        data: { type: Object, required: true },
+        // TODO: Audit features fields.
         features: [{
             properties: {type: Object, required: true},
             style: {
@@ -69,6 +21,7 @@ const MapSchema = new Schema(
                 },
             }
         }],
+        // TODO: Audit graphics fields.
         graphics: {
             fontStyle: { type: String, required: true },
             fontSize: { type: Number, required: true },
@@ -95,15 +48,13 @@ const MapSchema = new Schema(
         },
         social: {
             views: { type: Number, required: true },
-            likes: { type: Number, required: true },
-            dislikes: { type: Number, required: true },
+            likes: [Schema.Types.ObjectId],
+            dislikes: [Schema.Types.ObjectId],
             comments: [
                 {
-                    comment: String,
-                    userReference: String,
-                    username: String,
-                    userInitial: String,
-                    datePublished: Date
+                    comment: { type: String, required: true },
+                    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+                    date: { type: Date, required: true },
                 }
             ]
         }
