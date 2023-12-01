32709f1124d2a06620cd0102942769597fcd3999
Orson <55061548+0carb0n@users.noreply.github.com>
2023-11-30 20:52:14 -0500

    Styling.

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -7,16 +7,16 @@ const api = axios.create({
     baseURL: `${import.meta.env.VITE_SERVER_URL}/api`
 })
 
-const postCreateMap = (data) => api.post("/map", data);
-const getCurrentMap = (id) => api.get(`/map/${id}`);
 const getMaps = (view, searchText, searchBy, username) => {
-    return api.post(`/maps`, {
+    return api.get(`/maps`, {
         view: view,
         searchText: searchText,
         searchBy: searchBy,
         username: username
     })
 }
+const postCreateMap = (data) => api.post("/map", data);
+const getCurrentMap = (id) => api.get(`/map/${id}`);
 const updateMap = (id, data) => {
     return api.put(`/map/${id}`, {
         map: data
