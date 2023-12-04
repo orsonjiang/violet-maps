8bdb8dae0d5f36c8618a89969595840ee1563ae8
Orson <55061548+0carb0n@users.noreply.github.com>
2023-12-03 22:08:51 -0500

    Removed req limits on server.

--- server/index.js
+++ server/index.js
@@ -9,22 +9,24 @@ dotenv.config();
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-// TODO: Investigate limit.
-
 // SETUP THE MIDDLEWARE
-app.use(express.urlencoded({ extended: true, limit: '200mb' }))
+// TODO: Remove this.
+// app.use(express.urlencoded({ extended: true, limit: '200mb' }))
+app.use(express.urlencoded())
 app.use(cors({
     origin: [process.env.CLIENT_URL],
     credentials: true
 }))
-app.use(express.json({ limit: '200mb' }));
+// TODO: Remove this.
+// app.use(express.json({ limit: '200mb' }));
+app.use(express.json());
 app.use(cookieParser());
 
 // SETUP OUR OWN ROUTERS AS MIDDLEWARE
-const authRouter = require('./routes/auth-router')
+const authRouter = require('./routes/authRouter')
 app.use('/auth', authRouter)
 
-const apiRouter = require('./routes/api-router')
+const apiRouter = require('./routes/apiRouter')
 app.use('/api', apiRouter)
 
 // INITIALIZE OUR DATABASE OBJECT
