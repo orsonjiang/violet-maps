66d84e953e57b6deed73c2678556600a8b02d9cb
Orson <55061548+0carb0n@users.noreply.github.com>
2023-12-12 13:18:21 -0500

    Removed all tests to prepare for new infrastructure.

--- tests/frontend/cypress/e2e/layout.cy.js
+++ tests/frontend/cypress/e2e/layout.cy.js
@@ -1,4 +1,4 @@
-describe('create, read, update, and delete comment', () => {
+describe('view site layout', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5173/');
   })
@@ -10,73 +10,4 @@ describe('create, read, update, and delete comment', () => {
       cy.contains('Continue as Guest');
     })
   })
-
-  describe('view sign up screen', () => {
-    it('click and view sign up screen', () => {
-      cy.contains('Sign Up').click();
-      cy.contains('Create your new account');
-      cy.contains('Sign Up');
-      cy.url().should('include', 'register');
-    })
-  })
-
-  describe('view log in screen', () => {
-    it('click and view log in screen', () => {
-      cy.contains('Log In').click();
-      cy.contains('Sign in to your account');
-      cy.contains('Forgot password?');
-      cy.url().should('include', '/login');
-    })
-  })
-
-  describe('request reset password', () => {
-    it('click and view login then request reset password screen', () => {
-      cy.contains('Log In').click();
-      cy.contains('Forgot password?').click();
-      cy.contains('Reset your password');
-      cy.url().should('include', '/requestReset');
-    })
-  })
-
-  describe('reset password', () => {
-    it('view reset password screen', () => {
-      cy.visit('http://localhost:5173/reset/');
-      cy.get('input').should('have.length', 2);
-    })
-  })
-
-  describe('visit explore' , () => {
-    it('view explore screen from splash', () => {
-      cy.contains('Continue as Guest').click();
-      cy.url().should('include', '/app/home');
-      cy.contains('All Maps');
-      cy.contains('Sort By').click();
-      cy.contains('Name');
-    })
-  })
-
-  // describe('visit selected map page', () => {
-  //   it('view the selected map screen', () => {
-  //     cy.contains('Continue as Guest').click();
-  //     cy.url().should('include', '/app/home');
-  //     cy.contains('Choropleth Map').click();
-  //     cy.contains('Choropleth Map');
-  //     // cy.contains('America');
-  //   })
-  // })
-  /*
-  describe('visit selected map page', () => {
-    it('view the selected map screen', () => {
-      cy.contains('Continue as Guest').click();
-      cy.url().should('include', '/app/home');
-      cy.contains('This is one great map').click();
-      cy.url().should('include', '/app/map');
-      cy.contains('great');
-      cy.contains('Export').click();
-      cy.contains('JSON');
-      cy.contains('0 Comments');
-    })
-  })
-  */
-  
 })
--- tests/frontend/cypress/support/e2e.js
+++ tests/frontend/cypress/support/e2e.js
@@ -15,11 +15,5 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
-import 'cypress-plugin-api';
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from failing the test
-  return false
-})
