8b2a3446c0a1f8addc7d13dfca0fa4041ee1291d
Kevin Chen <kevin.chen.15@stonybrook.edu>
2023-12-18 03:40:35 -0500

    Implemented Sorting of Maps

--- client/src/pages/AppPages/components/SortBy.jsx
+++ client/src/pages/AppPages/components/SortBy.jsx
@@ -1,4 +1,66 @@
-const SortBy = ({ menu, setMenu }) => {
+import { useState, useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMaps } from '../../../actions/maps';
+
+const SortBy = () => {
+    // Kevin - new code
+    const { maps } = useSelector((state)=> state.maps);
+    const dispatch = useDispatch();
+    const ref = useRef(null);
+    const [menu, setMenu] = useState('none');
+
+    
+    const handleClickSortBy = (s) => {
+        handleSort(s, maps);
+    }
+    
+    const closeMenus = (ref) => {
+        useEffect(() => {
+            function handleClickOutside(event) {
+                if (ref.current && !ref.current.contains(event.target)) {
+                    setMenu('none');
+                }
+            }
+            document.addEventListener('mousedown', handleClickOutside);
+            return () => {
+                document.removeEventListener('mousedown', handleClickOutside);
+            };
+        }, [ref]);
+    };
+    closeMenus(ref);
+
+    const handleSort = (sortBy, curr_maps) => {
+    
+        let maps_list = curr_maps;
+        switch (sortBy) {
+            case "none":
+                // default order of maps is based on chronological order oldest at top and newest to bottom
+                maps_list.sort((a,b) => (new Date (a.createdAt) - new Date (b.createdAt)));
+                break;
+            case "name":
+                // numerical values take precendence over alphabetical values
+                maps_list.sort((a,b) => (a.name.localeCompare(b.name, undefined, { sensitivity: 'base'})));
+                break;
+            
+            case "date":
+                maps_list.sort((a,b) => (new Date (a.createdAt) - new Date (b.createdAt)));
+                break;
+            
+            case "likes":
+                maps_list.sort((a,b) => a.social.likes < b.social.likes ? 1 : -1);
+                break;
+
+            case "dislikes":
+                maps_list.sort((a,b) => a.social.dislikes < b.social.dislikes ? 1 : -1);
+                break;
+
+            default:
+                console.log("error established from handlesort");
+                break;
+        };
+        dispatch(setMaps(maps_list));
+    };
+    
     // TODO: Fix menu.
     return (
         <div className="relative">
@@ -42,6 +104,7 @@ const SortBy = ({ menu, setMenu }) => {
                             <button
                                 type="button"
                                 className="inline-flex w-full px-4 py-2 hover:bg-gray-100 "
+                                onClick={() => handleClickSortBy("none")}
                             >
                                 None
                             </button>
@@ -50,6 +113,7 @@ const SortBy = ({ menu, setMenu }) => {
                             <button
                                 type="button"
                                 className="inline-flex w-full px-4 py-2 hover:bg-gray-100 "
+                                onClick={() => handleClickSortBy("name")}
                             >
                                 Name
                             </button>
@@ -58,6 +122,7 @@ const SortBy = ({ menu, setMenu }) => {
                             <button
                                 type="button"
                                 className="inline-flex w-full px-4 py-2 hover:bg-gray-100 "
+                                onClick={() => handleClickSortBy("date")}
                             >
                                 Creation Date
                             </button>
@@ -66,6 +131,7 @@ const SortBy = ({ menu, setMenu }) => {
                             <button
                                 type="button"
                                 className="inline-flex w-full px-4 py-2 hover:bg-gray-100 "
+                                onClick={() => handleClickSortBy("likes")}
                             >
                                 Likes
                             </button>
@@ -74,6 +140,7 @@ const SortBy = ({ menu, setMenu }) => {
                             <button
                                 type="button"
                                 className="inline-flex w-full px-4 py-2 hover:bg-gray-100 "
+                                onClick={() => handleClickSortBy("dislikes")}
                             >
                                 Dislikes
                             </button>
