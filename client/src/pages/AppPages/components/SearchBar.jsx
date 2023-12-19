1d853e763b63143a71b62fbf79e0d72f073919ab
Orson <55061548+0carb0n@users.noreply.github.com>
2023-12-19 00:36:08 -0500

    Styling.

--- client/src/pages/AppPages/components/SearchBar.jsx
+++ client/src/pages/AppPages/components/SearchBar.jsx
@@ -6,11 +6,11 @@ const SearchBar = () => {
     const dispatch = useDispatch();
 
     return (
-        <div className="flex w-full border h-10 rounded bg-white">
+        <div className="flex w-full border h-10 rounded-full bg-white">
             {/* <div className="border px-3 flex items-center bg-slate-300">
             </div> */}
 
-            <input className="grow px-4 outline-none" placeholder="Search Maps" onChange={(event) => dispatch(setSearchText(event.target.value))}>
+            <input className="rounded-full grow px-4 outline-none" placeholder="Search Maps" onChange={(event) => dispatch(setSearchText(event.target.value))}>
 
             </input>
 
