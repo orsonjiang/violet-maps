e48ba37937f02fc1ab28bf18429bca6bf3e0a7b7
Kayla Fang <kayfang182@gmail.com>
2023-12-19 00:06:53 -0500

    split up selected map screen

---
+++ client/src/pages/AppPages/Map/components/Tag.js
@@ -0,0 +1,9 @@
+const Tag = ({name}) => {
+    return(
+        <div className=" bg-violet-200 focus:outline-none rounded-full px-4 py-1.5 text-center mb-2">
+            {name}
+        </div>
+    )
+}
+
+export default Tag;
