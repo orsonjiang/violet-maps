c163812c3e8fc10384505f5900d001f695d1b628
Orson <55061548+0carb0n@users.noreply.github.com>
2023-12-20 16:33:22 -0500

    Changed forking.

--- client/src/pages/AppPages/Modals/ForkMap.jsx
+++ client/src/pages/AppPages/Modals/ForkMap.jsx
@@ -22,7 +22,8 @@ const ForkMap = () => {
         apis.forkMap(map._id, name)
             .then((res) => {
                 closeModal(dispatch);
-                navigate(`/app/edit/${res.data.id}`);
+                // navigate(`/app/edit/${res.data.id}`);
+                navigate(`/app/home`);
             })
     };
 
